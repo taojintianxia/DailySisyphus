7604bc79d9a99dc2677d769071b0c13ed11787aa
Kane.Sun <taojintianxia@126.com>
2014-04-22 07:34:25 +0800

    improve the test demo

--- src/jbehave/pages/BaiduPage.java
+++ src/jbehave/pages/BaiduPage.java
@@ -1,10 +1,15 @@
 package jbehave.pages;
 
+import java.util.List;
+
 import jbehave.base.BasePage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 /**
- *
- *
+ * 
+ * 
  * @author Kane.Sun
  * @version Apr 18, 2014 3:58:50 PM
  * 
@@ -12,13 +17,27 @@ import jbehave.base.BasePage;
 
 public class BaiduPage extends BasePage {
 
+	private WebElement searchField;
+	private WebElement searchButton;
+
 	public void openHomePage() {
 		openUrl("/");
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+	}
+
+	public void searchText() {
+		searchField = findElementBy(By.id("kw1"));
+		searchButton = findElementBy(By.id("su1"));
+		typeTextBox(searchField, "Jbehave");
+		clickOn(searchButton);
+	}
+
+	public boolean isSearchResultAppear() {
+		return findElementBy(By.id("content_left")) != null;
+	}
+
+	public void nevigateLinkRandomly() {
+		List<WebElement> links = findElementsBy(By.cssSelector(".t a"));
+		clickOn(links.get(random.nextInt(links.size())));
 	}
 
 }
--- src/jbehave/steps/TestSteps.java
+++ src/jbehave/steps/TestSteps.java
@@ -1,5 +1,6 @@
 package jbehave.steps;
 
+import static org.junit.Assert.assertTrue;
 import jbehave.base.BaseStep;
 import jbehave.pages.BaiduPage;
 
@@ -10,8 +11,8 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
- *
- *
+ * 
+ * 
  * @author Kane.Sun
  * @version Apr 18, 2014 3:50:08 PM
  * 
@@ -30,16 +31,16 @@ public class TestSteps extends BaseStep {
 
 	@When("I search text on Baidu")
 	public void iSearchTextOnBaidu() {
-
+		baiduPage.searchText();
 	}
 
 	@Then("I should see the search result on baidu")
 	public void iShouldSeeTheSearchResultOnBaidu() {
-
+		assertTrue(baiduPage.isSearchResultAppear());
 	}
 
 	@When("I nevigate a result link on baidu randomly")
 	public void iNevigateAResultLinkOnBaiduRandomly() {
-
+		baiduPage.nevigateLinkRandomly();
 	}
 }
